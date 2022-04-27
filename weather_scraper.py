3f46cbb83197016033679273594bbebf38041089
hmccormak <hmccormak@protonmail.com>
2022-04-26 21:32:36 -0400

    fix

--- weather_scraper.py
+++ weather_scraper.py
@@ -4,33 +4,6 @@ from dataclasses import dataclass
 from bs4 import BeautifulSoup
 from geopy.geocoders import Nominatim
 
-def get_search(zip):
-    geolocator = Nominatim(user_agent='weatherbot')
-    coords = geolocator.geocode(zip, country_codes='us')
-    url = f'https://forecast.weather.gov/MapClick.php?lat={coords.latitude}&lon={coords.longitude}'
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, "html.parser")
-    
-    get_current(soup)
-
-def get_current(soup):
-    cur_cond = soup.find(class_ = 'myforecast-current').string
-    cur_far = soup.find(class_ = 'myforecast-current-lrg').string
-    cur_cel = soup.find(class_ = 'myforecast-current-sm').string
-    
-    cur_detail = []
-    for data in soup.findAll('td'):
-        cur_detail.append(data.string)
-
-    cur_detail[11] = cur_detail[11].replace("\n", '')
-    cur_detail[11] = cur_detail[11].strip()
-    
-    search_current_data =  current_data(
-    cur_cond, cur_far, cur_cel, cur_detail[1], cur_detail[3], 
-    cur_detail[5], cur_detail[7], cur_detail[9], cur_detail[11]
-    )
-    print(search_current_data)
-
 @dataclass   
 class current_data:
     cond: str = None
@@ -43,8 +16,41 @@ class current_data:
     vis: str = None
     last: str = None
 
+def get_search(zip):
+    geolocator = Nominatim(user_agent='weatherbot')
+    coords = geolocator.geocode(zip, country_codes='us')
+    url = f'https://forecast.weather.gov/MapClick.php?lat={coords.latitude}&lon={coords.longitude}'
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
+    
+    get_current(soup)
+    get_extended(soup)
+
+def get_current(soup):
+    cur_summary = []
+    for data in soup.find(id = 'current_conditions-summary'):
+        if data.string != '\n':
+            if data.string != None:
+                cur_summary.append(data.string)
+    
+    cur_detail = []
+    for data in soup.findAll('td'):
+        cur_detail.append(data.string)
+
+    cur_detail[11] = cur_detail[11].replace("\n", '')
+    cur_detail[11] = cur_detail[11].strip()
+    
+    search_current_data =  current_data(
+    cur_summary[0], cur_summary[1], cur_summary[2], cur_detail[1], 
+    cur_detail[3], cur_detail[5], cur_detail[7], cur_detail[9], cur_detail[11]
+    )
+    
+def get_extended(soup):
+    for data in soup.findAll(class_ = 'short-desc'):
+        print(data.string)
+
 def main(zip):
-    weather_data_raw = get_search(zip)
+    weather_data = get_search(zip)
 
 
 main('42069')
